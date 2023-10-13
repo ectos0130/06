933c5d8ecc3aa5e20e564d41d128e530f5ffa174
ectos0130 <ectos0130@naver.com>
2023-10-13 19:24:40 +0900

    5

--- main.c
+++ main.c
@@ -2,13 +2,37 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int square(int a){
-	return a*a;
+int factorial(int num){
+	int res = 1;
+	int i;
+	for (i = 1; i <= num; i++){
+		res=res*i;
+	}
+	return res;
 }
+
+int combination(int n, int r){
+	int div1, div2;
+	
+	div1 = factorial(n);
+	div2 = factorial(n-r)*factorial(r);
+	                  
+	return div1/div2;
+}
+
+
 int main(int argc, char *argv[]) {
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n",a);
+	int n, r;
+	
+	printf("input n : ");
+	scanf("%d", &n);
+	
+	printf("input r : ");
+	scanf("%d", &r);
+	
+	if (n >= 0 && r >=0 && n >= r){
+		printf("combination result : %i\n", combination(n,r));
+	}
 	
 	return 0;
 }
